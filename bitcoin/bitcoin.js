816ee73c104be64b70ae201f6b05ec5856749cd5
tordar <tordar.tommervik@gmail.com>
2021-02-06 19:01:40 +0100

    Some updates

--- bitcoin/bitcoin.js
+++ bitcoin/bitcoin.js
@@ -1,3 +1,5 @@
+/* 
+
 fetch('https://api.cryptonator.com/api/ticker/btc-usd')
     .then(res => {
         console.log("Response, waiting to parse", res)
@@ -14,6 +16,45 @@ fetch('https://api.cryptonator.com/api/ticker/btc-usd')
     .catch(err => {
         console.log("There was an error", err)
     })
+ */
 
 // This is much much better. It uses the new Fetch API, with callbacks. It still doesn't use the async functions though, which are
 // even easier to deal with.
+
+
+/*
+axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+    .then(res => {
+        console.log(res.data.ticker.price)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+ */
+// Here we use the Axios library to create a simplified version of the previous function. It is better because we do not have to parse
+// the Json files ourselves, but immediately get our headers, ready to use. We also do not need to use the fetch function, because 
+// Axios does all the heavy lifting for us.
+
+
+
+const fetchBitcoinPrice = async () => {
+    try {
+        const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+        console.log(res.data.ticker.price)
+        return res.data.ticker.price
+    }
+    catch (e) {
+        console.log("There has been an error", e)
+    }
+}
+const addBtc = async () => {
+    const newPrice = await fetchBitcoinPrice()
+    let priceP = document.querySelector('#ticker')
+    priceP.innerHTML = `The current price for Bitcoin is ${newPrice}`
+}
+addBtc()
+
+// Pretty much exactly the same as the previous function, except it is bound within an async function. When we use async functions,
+// we are exempted from using .then keywords.
+
+
