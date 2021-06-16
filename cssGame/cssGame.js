7ae88af3523ed68be2201c3f00739f4e5ba16134
tordar <tordar.tommervik@gmail.com>
2021-06-16 13:23:10 +0200

    tile clicker 1.1

--- cssGame/cssGame.js
+++ cssGame/cssGame.js
@@ -1,20 +1,30 @@
+// POTENTIAL IMPROVEMENTS
+// Add difficulty levels, by changing the setTimeout function: fast, medium, slow
+
+
+// VARIABLE DECLARATIONS
 let container = document.getElementById('container')
 let start = document.getElementById('start')
 let startButton = document.getElementById('startButton')
+let CountButtonHomeClicks = 0;
+let timeOut
+let prevNum = 1
+let num = 1
 
+//FUNCTION DECLARATIONS 
 const startGame = () => {
     startButton.addEventListener('click', ()=>{
         styleAdder()
         start.remove()
     })
 }
-startGame()
 
 const gameOver = () => {
     let gameOver = document.createElement('div')
     document.body.appendChild(gameOver)
     gameOver.classList.add('gameover')
-    gameOver.innerText = "GAME OVER\nSTART AGAIN?"
+    gameOver.innerText = `YOU GOT A SCORE OF: ${CountButtonHomeClicks}\nSTART AGAIN?`
+    CountButtonHomeClicks = 0;
     container.remove()
     gameOver.addEventListener('click', () =>{
         document.body.appendChild(container)
@@ -23,10 +33,6 @@ const gameOver = () => {
     })
 }
 
-let prevNum = 1
-let num = 1
-
-let timeOut
 const resetTimer = () =>{
     window.clearTimeout(timeOut);
     timeOut = setTimeout(
@@ -47,6 +53,8 @@ const styleAdder = () =>{
             x.classList.add('style');
             }
     x.addEventListener('click', ()=>{
+        CountButtonHomeClicks += 1
+        console.log(CountButtonHomeClicks)
         if(x.classList.contains('style')){
             styleAdder()
             x.classList.remove('style')
@@ -55,6 +63,5 @@ const styleAdder = () =>{
     })
 }
 
-
-
-
+// INITIALISE GAME
+startGame()
